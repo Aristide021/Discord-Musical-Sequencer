7a36df0b54f8b69a9438f075f62ae367ba50d1f2
Sheldon Aristide <44308312+Aristide021@users.noreply.github.com>
2022-12-22 09:53:06 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+  intents: [
+    GatewayIntentBits.ALL
+    // ...
+  ]
+})
+
+// Create an array to hold the timeline of musical events
+let timeline = [];
+
+client.on('message', message => {
+  // Check if the message starts with a slash
+  if (message.content[0] === '&') {
+    // Split the message into separate words
+    const words = message.content.split(' ');
+
+    // Check if the first word is "add"
+    if (words[0] === '&add') {
+      // Check if the second word is a valid musical event type
+      if (words[1] === 'drum' || words[1] === 'synth' || words[1] === 'guitar') {
+        // Check if the third word is a valid measure number
+        if (Number.isInteger(parseInt(words[2]))) {
+          // Add the musical event to the timeline
+          timeline.push({
+            type: words[1],
+            measure: parseInt(words[2]),
+            data: words.slice(3).join(' ')
+          });
+
+          // Sort the timeline by measure number
+          timeline.sort((a, b) => a.measure - b.measure);
+
+          // Reply to the user to confirm the event was added
+          message.reply(`Added ${words[1]} event at measure ${words[2]}`);
+        } else {
+          // Reply to the user with an error message
+          message.reply('Invalid measure number');
+        }
+      } else {
+        // Reply to the user with an error message
+        message.reply('Invalid event type');
+      }
+    }
+  }
+});
+
+client.login('BOT_TOKEN');
